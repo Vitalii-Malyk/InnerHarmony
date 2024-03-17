668bc8248f04322b416ebb7df5df796831efcae4
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-03-17 18:56:26 +0100

    fix home styles

--- src/components/Home/Home.styled.js
+++ src/components/Home/Home.styled.js
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import { Container } from "styles/GlobalStyles";
+import { Container } from "../../styles/GlobalStyles";
 
 export const StyledContainer = styled(Container)`
   padding-top: 78px;
