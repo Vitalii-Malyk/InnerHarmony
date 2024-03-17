35207ed9a0122ab17b24f04704a845b8a0012bbc
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-03-17 14:25:54 +0100

    fix theme & styles

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    mainGreen: "#54BE96",
+    mainGreenHover: "#36A379",
+    white: "#FBFBFB",
+    black: "#191A15",
+    lightWhite: "rgba(251, 251, 251, 0.50)",
+    gray: "rgba(25, 26, 21, 0.50)",
+    violet: "#4535AF",
+    yellow: "#FBC75E",
+    borderGray: "rgba(25, 26, 21, 0.10)",
+    boldGray: "#8A8A89",
+    backgroundGray: "#f3f3f3",
+    green: "#38CD3E",
+    backgroundLightGreen: "rgba(84, 190, 150, 0.20)",
+    star: "#FFC531",
+  },
+};
