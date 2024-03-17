5037feae3d6922a316c25620acfb2407801e18f2
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-03-17 15:04:55 +0100

    fonts

---
+++ src/styles/fonts/index.js
@@ -0,0 +1,13 @@
+import fontRegular from "./Inter-Regular.ttf";
+import fontMedium from "./Inter-Medium.ttf";
+import fontSemiBold from "./Inter-SemiBold.ttf";
+import fontBold from "./Inter-Bold.ttf";
+
+const fontObject = {
+  regular: fontRegular,
+  medium: fontMedium,
+  semiBold: fontSemiBold,
+  bold: fontBold,
+};
+
+export default fontObject;
