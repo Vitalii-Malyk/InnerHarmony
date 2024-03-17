04fbbf3a4f4005fd1e796d72a804283bb592e77a
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-03-17 18:52:26 +0100

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,35 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import { App } from "./components/App";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-// import "./index.css";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./firebase";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
+  <ThemeProvider theme={theme}>
+    <BrowserRouter basename="/InnerHarmony">
+      <Provider store={store}>
+        <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <GlobalStyles />
+      </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </ThemeProvider>
 );
